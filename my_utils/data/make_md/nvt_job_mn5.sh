9e1b5e195034ed95465f01cb227bf7d71e1f3932
chewingram <longo.samuel@outlook.it>
2024-11-19 15:49:51 +0100

    g

---
+++ my_utils/data/make_md/nvt_job_mn5.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+# Submission script for MareNostrum5
+#SBATCH --job-name=NPT
+#SBATCH --output=%j.out
+#SBATCH --error=%j.err
+#SBATCH --ntasks=
+#SBATCH --cpus-per-task=1
+#SBATCH --time=
+#SBATCH --partition=gpp
+#SBATCH --qos=gp_ehpc
+#SBATCH --account=ehpc14
+#SBATCH --requeue
+
+exec > ${SLURM_SUBMIT_DIR}/${SLURM_JOB_NAME}_${SLURM_JOBID}.log
+echo "------------------ Work dir --------------------"
+cd ${SLURM_SUBMIT_DIR} && echo ${SLURM_SUBMIT_DIR}
+echo "------------------ Job Info --------------------"
+echo "jobid : $SLURM_JOBID"
+echo "jobname : $SLURM_JOB_NAME"
+#echo "job type : $PBS_ENVIRONMENT"  I CANNOT FIND THE SLURM ANALOGUE
+echo "submit dir : $PWD"
+echo "queue : $SLURM_JOB_PARTITION"
+echo "user : $SLURM_JOB_USER"
+echo "threads : $OMP_NUM_THREADS"
+
+
+####################################################################################
+# Load modules and python environment
+module --force purge
+source /gpfs/home/ulie/ulie583683/environments/intel_2023.2.0
+
+export OMP_NUM_THREADS=1
+export MKL_NUM_THREADS=1
+
+echo -n "The job started on "
+date
+####################################################################################
+# The actual job
+python RunNVT.py
+
+echo -n "The job finished on "
+date
+
