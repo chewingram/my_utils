2c63e8d2f58ffaad553a898dc797cbb1c0ec9327
chewingram <122277684+chewingram@users.noreply.github.com>
2024-11-22 09:56:25 +0100

    Update utils_md.py

--- my_utils/utils_md.py
+++ my_utils/utils_md.py
@@ -198,16 +198,17 @@ def run_md(ismpi = False,
         # file in which we can store all the configurations we got wit the MD along with their energy computes with the
         # calculator.
         elif not wdir.joinpath(f'T{temperature}K.traj').exists():
-            newtraj = Trajectory(wdir.joinpath(f'T{temperature}K.traj').absolute(), 'w')
+            newtraj = reconstruct_mlmd_trajectory(workdir.joinpath('Trajectory/mlmd.traj').absolute(), workdir.joinpath('Trajectory/mlmd.log').absolute())[nthrow:]
+            write(wdir.joinpath(f'T{temperature}K.traj').absolute(), newtraj)
             #traj = reconstruct_mlmd_trajectory(f'{workdir}mlmd.traj', f'{workdir}mlmd.log')[nthrow:]
-            os.environ["ASE_LAMMPSRUN_COMMAND"] = f"{mpirun} -n {nproc} {lmp_bin.absolute()}"
-            calc = LAMMPS(pair_style=pair_style, 
-                          pair_coeff=pair_coeff,
-                          tmp_dir=Path('WTFASE').absolute())
-            for at in traj:
-                at.calc = calc
-                at.get_potential_energy()
-                newtraj.write(at)
+            #os.environ["ASE_LAMMPSRUN_COMMAND"] = f"{mpirun} -n {nproc} {lmp_bin.absolute()}"
+            #calc = LAMMPS(pair_style=pair_style, 
+            #              pair_coeff=pair_coeff,
+            #              tmp_dir=Path('WTFASE').absolute())
+            #for at in traj:
+            #    at.calc = calc
+            #    at.get_potential_energy()
+            #    newtraj.write(at)
         else:
             pass
 
